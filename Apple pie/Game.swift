3cbc77bf801571203f82a2633495e1835812a88b
Rokyah18 <75707959+Rokyah18@users.noreply.github.com>
2020-12-15 19:31:19 -0600

    Initial commit

---
+++ Apple pie/Game.swift
@@ -0,0 +1,39 @@
+//
+//  Game.swift
+//  Apple pie
+//
+//  Created by Rokyah Hamilton on 12/12/20.
+//
+
+import Foundation
+
+struct Game {
+    
+    
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed (letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains (letter) {
+            incorrectMovesRemaining -= 1
+        }
+        
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)" } else {
+                    guessedWord += "-"
+            }
+        }
+        return guessedWord
+    }
+}
+
+
+
+
